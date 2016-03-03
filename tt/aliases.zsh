73b12fa6a41c98d5d8ba9d6809cd3d51eb81e995
eldh <andreas.eldh@gmail.com>
2016-03-03 10:46:54 +0100

    more tt aliases

--- tt/aliases.zsh
+++ tt/aliases.zsh
@@ -1,2 +1,4 @@
 alias thq="ssh tictailhq.com"
 alias ops="ssh ops.production.tictail.com"
+alias msm="make serve-marketplace"
+alias mss="HOSTNAME=bekkibraunberlin.tictail.com make serve-storefront"
