05e97f51f805f2bd3fd2b62dfb9a7fbc5d90d93b
eldh <andreas.eldh@gmail.com>
2016-04-13 14:47:21 +0200

    stuff

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,6 +1,9 @@
 # matches case insensitive for lowercase
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
+zstyle ':completion:*' accept-exact '*(N)'
+zstyle ':completion:*' use-cache on
+
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 
