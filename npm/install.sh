7ec79ead8adc5b099e87de965495f000b5c2f253
eldh <andreas.eldh@gmail.com>
2016-08-05 12:32:10 +0200

    diff-so-fancy

--- npm/install.sh
+++ npm/install.sh
@@ -6,6 +6,7 @@ npm install -g eslint
 npm install -g webpack
 npm install -g babel
 npm install -g react-native-cli
+npm install -g diff-so-fancy
 npm install -g jsdom
 npm install -g node-inspector
 npm install -g np
