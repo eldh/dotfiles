9d547b8ab72ef9a8b3e553462515bec917c498e3
eldh <andreas.eldh@gmail.com>
2017-07-13 23:14:56 +0200

    Updates

--- config.symlink/fish/config/env.fish
+++ config.symlink/fish/config/env.fish
@@ -1,10 +1,13 @@
 set -x NODE_PATH /usr/local/lib/node
-set -x EDITOR atom -nw
+set -x EDITOR ne
+set -x DOTFILES ~/dotfiles
 
 # PATH Setup
 
 # Languages
 set PATH /usr/local/sbin $PATH # homebrew
+set PATH /Library/Frameworks/Python.framework/Versions/2.7/bin $PATH # python
 set PYTHONPATH /usr/local/lib/python2.7/site-packages $PYTHONPATH
+set PYTHONPATH /Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages $PYTHONPATH
 
 set PATH $DOTFILES/bin $PATH # local bins
--- npm/install.sh
+++ npm/install.sh
@@ -3,14 +3,14 @@
 npm install -g fast-cli
 npm install -g npm-check-updates
 npm install -g eslint
+npm install -g jest
 npm install -g webpack
-npm install -g babel
-npm install -g react-native-cli
 npm install -g diff-so-fancy
-npm install -g jsdom
 npm install -g node-inspector
 npm install -g np
 npm install -g create-react-app
 npm install -g git-recent
+npm install -g vtop
+npm install -g http-server
 
 exit 0
