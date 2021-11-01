8fe3e1d81c2b7676c1557e51b72ac90665f1ea1a
eldh <612703+eldh@users.noreply.github.com>
2021-11-01 22:40:13 +0100

    New laptop

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -73,44 +73,44 @@ brew install scc
 brew install cloc
 
 brew tap homebrew/cask-cask
-brew cask install alfred
-brew cask install kitty
-brew cask install brave-browser
-brew cask install bettertouchtool
-brew cask install spotify
-brew cask install slack
-brew cask install transmission
-brew cask install visual-studio-code
-brew cask install kap
-brew cask install 1password
-brew cask install gitup
-brew cask install vlc
+brew install alfred
+brew install kitty
+brew install brave-browser
+brew install bettertouchtool
+brew install spotify
+brew install slack
+brew install transmission
+brew install visual-studio-code
+brew install kap
+brew install 1password
+brew install gitup
+brew install vlc
 
-brew cask install safari-technology-preview
+brew install safari-technology-preview
 
 brew tap homebrew/cask-fonts
-brew cask install font-inconsolata
-brew cask install font-input
-brew cask install font-fira-sans
-brew cask install font-fira-code
-brew cask install font-hasklig
-brew cask install font-monoid
-brew cask install font-source-sans-pro
-brew cask install font-source-code-pro
-brew cask install font-lato
-brew cask install font-pacifico
-brew cask install font-abril-fatface
-brew cask install font-crimson-text
-brew cask install font-libre-baskerville
-brew cask install font-roboto-slab
-brew cask install font-roboto
-brew cask install font-roboto-mono
-brew cask install font-noto-sans
-brew cask install font-inter
-brew cask install font-noto-serif
-# brew cask install font-noto-mono
-brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package qlvideo
-brew cask cleanup
+brew install font-inconsolata
+brew install font-input
+brew install font-fira-sans
+brew install font-fira-code
+brew install font-hasklig
+brew install font-monoid
+brew install font-source-sans-pro
+brew install font-source-code-pro
+brew install font-lato
+brew install font-pacifico
+brew install font-abril-fatface
+brew install font-crimson-text
+brew install font-libre-baskerville
+brew install font-roboto-slab
+brew install font-roboto
+brew install font-roboto-mono
+brew install font-noto-sans
+brew install font-inter
+brew install font-noto-serif
+# brew install font-noto-mono
+brew install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql webpquicklook suspicious-package qlvideo
+brew cleanup
 
 # Remove outdated versions from the cellar.
 brew cleanup
